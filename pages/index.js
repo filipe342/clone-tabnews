cbab407fb64c69b4e14b482a40d346f90d93a532
Filipe Gabriel <filipecardosogabriel@gmail.com>
2023-10-08 21:19:06 -0300

    mofificar a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Olá !!!</h1>
+    return <h1>"Olá, amigo! O mesmo ocorreu comigo. Acredito que possa ter sido uma intermitência, pois agora está funcionando normalmente."</h1>
 }
 
 export default Home;
