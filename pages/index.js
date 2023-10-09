6635a82cf27167cf1b5776191553548730a86b51
Filipe Gabriel <filipecardosogabriel@gmail.com>
2023-10-08 21:25:14 -0300

    mofificar a frase01

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>"Olá, amigo! O mesmo ocorreu comigo. Acredito que possa ter sido uma intermitência, pois agora está funcionando normalmente."</h1>
+    return <h1>A lot is changing in the space industry every year and as machine learning advances and sophistication of robotic technology is refined even further. We can see there is a very feasible path forward.
+
+Fuente: https://citas.in/autores/sheyene-gerardi/?q=2112823</h1>
 }
 
 export default Home;
