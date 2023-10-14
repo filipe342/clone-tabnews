e772b4807097821cf4892e1462b748ebf0c82f0e
Filipe Gabriel <filipecardosogabriel@gmail.com>
2023-10-13 21:42:23 -0300

    adicionar arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 function Home() {
-    return <h1>A lot is changing in the space industry every year and as machine learning advances and sophistication of robotic technology is refined even further. We can see there is a very feasible path forward.
-
-Fuente: https://citas.in/autores/sheyene-gerardi/?q=2112823</h1>
+  return <h1>A lot is changing in the space industry every year and as machine learning advances and sophistication of robotic technology is refined even further. We can see there is a very feasible path forward</h1>
 }
 
-export default Home;
+export default Home; 
