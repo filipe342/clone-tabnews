4acde5620812d587b2d05061e1189f25236178d3
Filipe Gabriel <filipecardosogabriel@gmail.com>
2023-10-13 22:20:16 -0300

    adiciona scripts `lint:heck` e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>A lot is changing in the space industry every year and as machine learning advances and sophistication of robotic technology is refined even further. We can see there is a very feasible path forward</h1>
+  return (
+    <h1>
+      A lot is changing in the space industry every year and as machine learning
+      advances and sophistication of robotic technology is refined even further.
+      We can see there is a very feasible path forward
+    </h1>
+  );
 }
 
-export default Home; 
+export default Home;
